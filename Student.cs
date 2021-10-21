244149ffd3f1ebee37c8311f8557c273f961ddb7
Anthony5293 <92908956+Anthony5293@users.noreply.github.com>
2021-10-21 00:15:29 -0600

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week2Activity
+{
+    public class Student
+    {
+        Student stundent1 = new Student();
+
+        public Student()
+        {
+            student1.Introduce();
+            Console.WriteLine("");
+        }
+
+
+    }
+}
